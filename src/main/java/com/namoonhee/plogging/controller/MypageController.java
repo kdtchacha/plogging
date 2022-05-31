a04af0656f0a190d7424b68f8bc44ff99440c229
chacha <toddlf64@naver.com>
2022-05-31 13:56:00 +0900

    zxc

--- src/main/java/com/namoonhee/plogging/controller/MypageController.java
+++ src/main/java/com/namoonhee/plogging/controller/MypageController.java
@@ -18,8 +18,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MypageController {
@@ -29,7 +28,6 @@ public class MypageController {
 
     @Autowired
     UserRepository userRepository;
-
 
     @GetMapping(value = "/mypage")
     public String mypage(HttpSession session, Model model) {
@@ -84,35 +82,37 @@ public class MypageController {
     @PostMapping("/user_info/update")
     public String userInfoUpdate(HttpSession session, User user) {
 
+        // 세션에 연결된 유저 정보.
+        User suser = (User) session.getAttribute("user");
 
-        //세션에 연결된 유저 정보.
-        User suser = (User)session.getAttribute("user");
-     
-        //그 정보를 토대로 디비에 접속해서 유저 정보 한 줄을 가져옴.
+        // 그 정보를 토대로 디비에 접속해서 디비에 저장되어있는 유저 정보 한 줄을 가져옴.
         Optional<User> opt = userRepository.findById(suser.getId());
 
+        // 그 한 줄에서 정보를 빼온다.
         User dbuser = opt.get();
 
-        dbuser.setNickname(newnickname);
+        // 닉네임에 새로운 닉네임과 비밀번호를 세팅한다.
+        dbuser.setNickname(user.getNickname());
+        dbuser.setPwd(user.getPwd());
 
-        userRepository.save(nnickname);
+        // 그 새로운 닉네임과 비밀번호를 디비에 저장한다.
+        userRepository.save(dbuser);
 
 
-        
-        
-        System.out.println(suser.getEmail());
-        System.out.println(suser.getPwd());
-        System.out.println(suser.getNickname());
-        System.out.println(suser.getId());
+        // 세션에 새로 저장한 닉네임을 세팅해준다.(유저는 화분/디비유저는 심을 내용물)
+        session.setAttribute("user", dbuser);
+
+        // System.out.println(suser.getEmail());
+        // System.out.println(suser.getPwd());
+        // System.out.println(suser.getNickname());
+        // System.out.println(suser.getId());
 
         // userRepository.findById(id);
 
-        //suser 중에서도 nickname을 가져옴.
-        suser.getNickname();
+        // suser 중에서도 nickname을 가져옴.
+        // suser.getNickname();
 
-        
-        String a = user.getNickname();
-
+        // String a = user.getNickname();
 
         // User newNickname = user.getNickname();
 
@@ -121,7 +121,6 @@ public class MypageController {
         // newNickname.setNickname();
 
         // UserRepository.save(newNickname);
-
 
         return "mypage";
     }
