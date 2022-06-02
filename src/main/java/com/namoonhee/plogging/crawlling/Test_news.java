6ae3e39524fd4cc5fb98db36e2acb5903b84ad54
chacha <toddlf64@naver.com>
2022-06-02 14:02:07 +0900

    0602_mypage_new 새로 만들어서 수정 중_lg

--- src/main/java/com/namoonhee/plogging/crawlling/Test_news.java
+++ src/main/java/com/namoonhee/plogging/crawlling/Test_news.java
@@ -17,7 +17,7 @@ public class Test_news {
         = Jsoup.connect("https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%ED%94%8C%EB%A1%9C%EA%B9%85");
         try {
             Document doc = conn.get();
-            Elements eles = doc.select(".news_info");
+            Elements eles = doc.select("a.news_tit");
     
             for(Element ele : eles) {
                 System.out.println(ele.text());
