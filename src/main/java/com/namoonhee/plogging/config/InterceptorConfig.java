f9f866fdcb23ea45bb40e0dba8be175167f37b65
chacha <toddlf64@naver.com>
2022-06-03 15:43:24 +0900

    0603_인터셉터+뉴스_lg

---
+++ src/main/java/com/namoonhee/plogging/config/InterceptorConfig.java
@@ -0,0 +1,26 @@
+package com.namoonhee.plogging.config;
+
+import com.namoonhee.plogging.interceptor.Interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer{
+
+    @Autowired
+    private Interceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        
+        registry
+        .addInterceptor(interceptor)
+        .addPathPatterns("/mypage", "/activityform");
+
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+    
+}
---
+++ src/main/java/com/namoonhee/plogging/interceptor/Interceptor.java
@@ -0,0 +1,34 @@
+package com.namoonhee.plogging.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.namoonhee.plogging.model.User;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class Interceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+
+        HttpSession session = request.getSession();
+
+        User user = (User) session.getAttribute("user");
+
+        if (user == null) {
+            response.sendRedirect("/signin");
+            return false;
+        }
+        return true;
+    }
+
+}
