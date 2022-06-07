24b9a3923d00d20790bff0c35fc355e9ceba6132
chacha <toddlf64@naver.com>
2022-06-07 13:46:25 +0900

    0607_활동기록삭제_LG

--- src/main/java/com/namoonhee/plogging/service/ActivityServiceImpl.java
+++ src/main/java/com/namoonhee/plogging/service/ActivityServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.namoonhee.plogging.model.ActFile;
 import com.namoonhee.plogging.model.Activity;
 import com.namoonhee.plogging.repository.ActFileRepository;
+import com.namoonhee.plogging.repository.ActivityRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,10 +17,19 @@ public class ActivityServiceImpl implements ActivityService{
     @Autowired
     ActFileRepository actFileRepository;
 
+    @Autowired
+    ActivityRepository activityRepository;
+
     @Override
     public List<ActFile> download(Activity activity) {
       List<ActFile> fList = actFileRepository.findByActivity(activity);
         return fList;
+    }
+
+    @Override
+    public void delete_activity(Long id) {
+       activityRepository.deleteById(id);
+      
     }
     
 }
