16a457aa5e28deb213a8e7a2c084eb03b6a9fe9b
PgrmKMB <emdk789@naver.com>
2022-06-09 09:18:01 +0900

    플로깅 저장화면 조금 다듬음

--- src/test/java/com/namoonhee/plogging/PloggingApplicationTests.java
+++ src/test/java/com/namoonhee/plogging/PloggingApplicationTests.java
@@ -1,10 +1,25 @@
 package com.namoonhee.plogging;
 
+import com.namoonhee.plogging.repository.ActAnswerRepository;
+import com.namoonhee.plogging.repository.ActivityRepository;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PloggingApplicationTests {
+
+	@Autowired
+	ActAnswerRepository actAnswerRepository;
+
+	@Autowired
+	ActivityRepository activityRepository;
+
+	@Autowired
+	
+
+
 
 	@Test
 	void contextLoads() {
