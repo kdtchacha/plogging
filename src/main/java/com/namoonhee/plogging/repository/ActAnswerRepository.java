d99237e2b4e335c5ed5a7bf8071b3ab0cee5dc33
chacha <toddlf64@naver.com>
2022-06-11 13:31:07 +0900

    0611_어드민댓글완료_LG

--- src/main/java/com/namoonhee/plogging/repository/ActAnswerRepository.java
+++ src/main/java/com/namoonhee/plogging/repository/ActAnswerRepository.java
@@ -1,12 +1,13 @@
 package com.namoonhee.plogging.repository;
 
-import java.util.List;
+
 
 import com.namoonhee.plogging.model.ActAnswer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ActAnswerRepository extends JpaRepository<ActAnswer, Long>{
     
-    List<ActAnswer> findByUser_id(Long id);
 }
--- src/main/java/com/namoonhee/plogging/repository/ActivityRepository.java
+++ src/main/java/com/namoonhee/plogging/repository/ActivityRepository.java
@@ -6,7 +6,9 @@ import com.namoonhee.plogging.model.Activity;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
 
     List<Activity> findByUser_id(Long id);
