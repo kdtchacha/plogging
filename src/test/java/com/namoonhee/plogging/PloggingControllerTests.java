01013e46c52cec53c657eaf9805203969dc618a6
PgrmKMB <emdk789@naver.com>
2022-06-11 21:57:57 +0900

    레파지토리 테스트

---
+++ src/test/java/com/namoonhee/plogging/PloggingControllerTests.java
@@ -0,0 +1,15 @@
+package com.namoonhee.plogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class PloggingControllerTests {
+
+    @Test
+    void test1(){
+        
+    }
+    
+}
--- src/test/java/com/namoonhee/plogging/PloggingRepositoryTests.java
+++ src/test/java/com/namoonhee/plogging/PloggingRepositoryTests.java
@@ -1,10 +1,13 @@
 package com.namoonhee.plogging;
 
-import com.namoonhee.plogging.model.ActFile;
+import com.namoonhee.plogging.model.Activity;
+import com.namoonhee.plogging.model.User;
 import com.namoonhee.plogging.repository.ActAnswerRepository;
 import com.namoonhee.plogging.repository.ActFileRepository;
 import com.namoonhee.plogging.repository.ActivityRepository;
 import com.namoonhee.plogging.repository.UserRepository;
+
+import java.util.Date;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +32,25 @@ class PloggingRepositoryTests {
 
 
 	@Test
-	void contextLoads() {
+	void activityRepositoryCreateTest() {
+
+		Activity act = new Activity();
+		act.setActDistance("1234m");
+		act.setActMemo("test_memo");
+		act.setActName("test_name");
+		act.setActTime("xx:xx:xx");
+		act.setCreateDate(new Date());
+		
+		User user = new User();
+		user.setId(1L);
+		act.setUser(user);
+
+		activityRepository.save(act);
 	}
+
+
+	@Test
+	
+
 
 }
