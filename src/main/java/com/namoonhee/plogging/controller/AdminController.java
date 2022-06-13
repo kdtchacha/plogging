9eda277d09083ca90bf0c6a1886d0c9505add193
chacha <toddlf64@naver.com>
2022-06-13 18:23:19 +0900

    0613_관리자페이지삭제처리중_LG

--- src/main/java/com/namoonhee/plogging/controller/AdminController.java
+++ src/main/java/com/namoonhee/plogging/controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.namoonhee.plogging.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
 
 import com.namoonhee.plogging.model.ActAnswer;
 import com.namoonhee.plogging.model.Activity;
@@ -8,11 +11,16 @@ import com.namoonhee.plogging.model.User;
 import com.namoonhee.plogging.repository.ActAnswerRepository;
 import com.namoonhee.plogging.repository.ActivityRepository;
 import com.namoonhee.plogging.repository.UserRepository;
+import com.namoonhee.plogging.service.ActivityService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class AdminController {
@@ -25,6 +33,9 @@ public class AdminController {
 
     @Autowired
     ActAnswerRepository actAnswerRepository;
+
+    @Autowired
+    ActivityService activityService;
 
     @GetMapping("/adminmain")
     public String adminMain() {
@@ -55,5 +66,36 @@ public class AdminController {
 
         return actAnswers;
     }
+
+    @GetMapping("/admin_delete_activity")
+    public String adminDeleteActivityForm() {
+        return "admin_delete_activity";
+    }
+
+    @PostMapping("/admin_delete_activity")
+    public String adminDeleteActivity(@RequestParam Long id, HttpSession session) {
+        
+        User suser = (User) session.getAttribute("user");
+
+        if (suser.getNickname().equals("admin")) {
+            activityService.delete_activity(id);
+        }
+
+
+
+        Optional<User> opt = userService.signin(user);
+        String redir = "";
+        if (opt.get().getNickname().equals("admin")) {
+            userService.deleteAccount(user);
+            httpSession.invalidate();
+            redir = "redirect:/";
+        } else {
+            redir = "redirect:/mypage_new";
+        }
+
+
+        return "redirect:/adminmain";
+    }
+    
 
 }
